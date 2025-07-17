b2581a6c0c8cfe6fb78e0acb461d2ae02c8452ad
jciaffi <jciaffi@gmail.com>
2025-07-17 17:45:43 +0200

    ter

--- conf.py
+++ conf.py
@@ -8,7 +8,8 @@ release = '1.0'  # ou laisse vide si non versionné
 html_show_sourcelink = False
 html_title = "Audioliz Documentation"
 html_short_title = "Audioliz"
-html_theme = "furo"
+# html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
 
 # Configuration Read the Docs
 if os.getenv("READTHEDOCS", None) == "True":
@@ -19,13 +20,14 @@ else:
 
 # Répertoire contenant les fichiers statiques (logos, CSS personnalisée, etc.)
 html_static_path = ['_static']
-html_css_files = ['custom.css']
+# html_css_files = ['custom.css']
 # html_js_files = ['language-selector.js']
 
 # Extensions Sphinx utilisées
 extensions = [
     'sphinxcontrib.youtube',
     'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme',
 ]
 
 # Configuration pour l'internationalisation
@@ -34,28 +36,32 @@ locale_dirs = ['locale']        # où sont vos .po/.mo
 gettext_uuid = True             # pour des .pot stables
 gettext_compact = False
 
+html_theme_options = {
+    'language_selector': True,
+    'version_selector': False,
+    'flyout_display': False,
+}
+
 # Configuration multilingue pour Read the Docs
 if os.getenv("READTHEDOCS", None) == "True":
     # Récupération de la langue depuis l'environnement Read the Docs
     language = os.getenv("READTHEDOCS_LANGUAGE", "fr")
     
     # Configuration des langues supportées
-    html_theme_options = {
+    html_theme_options.update({
         'language_versions': {
             'en': 'English',
             'fr': 'Français',
         }
-    }
+    })
 else:
     # Configuration locale pour le développement
-    html_theme_options = {
+    html_theme_options.update({
         'language_versions': {
             'en': 'English',
             'fr': 'Français',
         }
-    }
-
-html_theme_options['language_selector'] = True
+    })
 
 # Configuration pour Weblate
 gettext_allow_fuzzy_translations = True
